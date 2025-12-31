67437dba3523ee0d8d52ab8734a08236cd7596cd
aztechofficial0-ops <aztechofficial0@gmail.com>
2025-12-31 20:09:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ function auth(req, res, next) {
   const decoded = Buffer.from(encoded, "base64").toString("utf8");
   const [user, pass] = decoded.split(":");
 
-  if (user === "slipt" && pass === "1234") {
+  if (user === "aztech" && pass === "786") {
     return next();
   }
 
